8ac57afcc1ed1a96ecdc8c6c63f09f8cea0dbe90
sumitthrita <sumitthrita@gmail.com>
2022-07-05 09:14:34 +0530

    Updates

---
+++ precache-manifest.780779847a29d1e934825b7dfab9c617.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc393d3c746632b229e507e1d8567fc",
+    "url": "/leinadocar/index.html"
+  },
+  {
+    "revision": "2737433251d17f7bf7d5",
+    "url": "/leinadocar/static/css/2.c1c45ef1.chunk.css"
+  },
+  {
+    "revision": "0a5a5c960a1fdee06466",
+    "url": "/leinadocar/static/css/main.e18abca6.chunk.css"
+  },
+  {
+    "revision": "2737433251d17f7bf7d5",
+    "url": "/leinadocar/static/js/2.8f5d9f5e.chunk.js"
+  },
+  {
+    "revision": "b6c269b6374f988b4fe55daf3bdb7082",
+    "url": "/leinadocar/static/js/2.8f5d9f5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8055e30e3878d3077a5",
+    "url": "/leinadocar/static/js/3.4db36a29.chunk.js"
+  },
+  {
+    "revision": "0a5a5c960a1fdee06466",
+    "url": "/leinadocar/static/js/main.2466b207.chunk.js"
+  },
+  {
+    "revision": "be30094412e0160a63d3",
+    "url": "/leinadocar/static/js/runtime-main.50b5c6b3.js"
+  },
+  {
+    "revision": "f63b921d8053c889bebe99a4102d6017",
+    "url": "/leinadocar/static/media/drumBrakeTuning.f63b921d.png"
+  },
+  {
+    "revision": "80e2e6f1582ea7fb883d6b75d0f19a43",
+    "url": "/leinadocar/static/media/frontBrakeDisk.80e2e6f1.png"
+  },
+  {
+    "revision": "9bf7bf1ffe2798bf269066be4ad0ffc6",
+    "url": "/leinadocar/static/media/radiatorFlushClean.9bf7bf1f.png"
+  },
+  {
+    "revision": "ba6cbcd715a40113dab162d490dbd946",
+    "url": "/leinadocar/static/media/radiatorReplace.ba6cbcd7.png"
+  },
+  {
+    "revision": "5e8923b651102e93535b3f274e0c28b0",
+    "url": "/leinadocar/static/media/sPBasic.5e8923b6.png"
+  },
+  {
+    "revision": "ba6cbcd715a40113dab162d490dbd946",
+    "url": "/leinadocar/static/media/sPComprehensive.ba6cbcd7.png"
+  },
+  {
+    "revision": "4b13d93a0478d6e38611bbda413823bf",
+    "url": "/leinadocar/static/media/sPStandard.4b13d93a.png"
+  }
+]);
